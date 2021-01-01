fda8c323435d86c9088da00b64889cf0ff6b2c3c
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-01-01 17:32:54 -0300

    set path for devise users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :planos
   resources :organizacoes
   resources :aseguradoras
-  devise_for :users
+  devise_for :users, path: 'u'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
