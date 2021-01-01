4614e5ec8680d2f39951699c1f8c1f975b3b822d
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-01-01 13:09:24 -0300

    added and configurted rspec with databasecleaner and factory_bot

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,14 @@ module Sanus
     config.generators do |generate|
       generate.assets false
       generate.helper false
-      generate.test_framework :test_unit, fixture: false
+      generate.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: true
+    generate.fixture_replacement :factory_bot, dir: "spec/factories"
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
