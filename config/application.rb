e22361916870104082c213fcd4a5cd2228f0b7b0
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-01-01 12:19:25 -0300

    config application for portuguese

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,12 @@ module Sanus
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.i18n.default_locale = :"pt-BR"
+    I18n.enforce_available_locales = false
+
+    config.time_zone = "Brasilia"
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
