8e766aa821652d76e4027a3727980978489235e1
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-01-03 16:14:49 -0300

    completed devise routes for beneficiarios and users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,21 @@ Rails.application.routes.draw do
   resources :planos
   resources :organizacoes
   resources :aseguradoras
-  devise_for :users, path: 'u', controllers: { sessions: "users/sessions" }
-  devise_for :beneficiarios, path: 'b', controllers: { sessions: "beneficiarios/sessions" }
+  devise_for :users, path: 'u', controllers: {
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    registrations: "users/registrations",
+    confirmations: "users/confirmations",
+    unlocks: "users/unlocks"
+  }
+
+  devise_for :beneficiarios, path: 'b', controllers: { 
+    sessions: "beneficiarios/sessions",
+    passwords: "beneficiarios/passwords",
+    registrations: "beneficiarios/registrations",
+    confirmations: "beneficiarios/confirmations",
+    unlocks: "beneficiarios/unlocks"
+  }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
