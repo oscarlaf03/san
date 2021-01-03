e8f2ad53d4fd075b690fc57db7b048ab9edbeba3
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-01-03 15:46:56 -0300

    adjusted sessions on sessions controller routes for beneficiarios

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :organizacoes
   resources :aseguradoras
   devise_for :users, path: 'u', controllers: { sessions: "users/sessions" }
-  devise_for :beneficiarios, path: 'b', controllers: { sessions: "b/sessions" }
+  devise_for :beneficiarios, path: 'b', controllers: { sessions: "beneficiarios/sessions" }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
