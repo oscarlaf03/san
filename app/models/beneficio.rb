5412ba85db1cf487ad6df7f1bc300939f54473d7
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-01-10 18:18:06 -0300

    added direct access to beneficio to plano and organizacao

--- app/models/beneficio.rb
+++ app/models/beneficio.rb
@@ -3,4 +3,7 @@ class Beneficio < ApplicationRecord
   belongs_to :organizacao_plano
   has_one :beneficio_condicao
   has_one :condicao, through: :beneficio_condicao
+  delegate :plano, to: :organizacao_plano, allow_nil: true
+  delegate :organizacao, to: :organizacao_plano, allow_nil: true
+
 end
--- spec/models/beneficio_spec.rb
+++ spec/models/beneficio_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Beneficio, type: :model do
   context "Relationships" do
-    it "tem uma condicao through beneficio_condicao" do
-      beneficio = create(:beneficio)
-      condicao = create(:condicao)
+    let(:beneficio) {create(:beneficio)}
+    let(:condicao) {create(:condicao)}
+    it "tem uma condicao through condicao_condicao" do
       create(:beneficio_condicao, beneficio: beneficio, condicao: condicao)
       expect(beneficio.condicao == condicao).to be true
+    end
+
+    it "Responde para plano retornando o Plano" do
+      expect(beneficio.plano.kind_of?(Plano)).to be true
+    end
+
+    it "Responde para organizacao retornando uma Organizacao" do
+      expect(beneficio.organizacao.kind_of?(Organizacao)).to be true
     end
 
   end
