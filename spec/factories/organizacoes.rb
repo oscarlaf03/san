27b5b7315d9f403210e0fe2058830e09786a29b1
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-01-17 17:01:21 -0300

    improved factory names

--- spec/factories/organizacoes.rb
+++ spec/factories/organizacoes.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :organizacao do
     slug { Faker::Company.buzzword }
-    razao_social { Faker::Company.name }
+    razao_social { Faker::Company.name + ' '+ Faker::Company.suffix }
     cnpj { Faker::Company.brazilian_company_number  }
   end
 end
