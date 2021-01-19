df3caa893904f6b1d68f4685fe4ab33bc9bd457f
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-01-18 22:24:26 -0300

    wip sidebar

---
+++ app/javascript/packs/components/sidebar.js
@@ -0,0 +1,9 @@
+export function toggleSideBar() {
+    $(document).ready(function() {
+
+        $('#sidebarCollapse').on('click', function() {
+            $('#sidebar').toggleClass('active');
+        });
+
+    })
+}
