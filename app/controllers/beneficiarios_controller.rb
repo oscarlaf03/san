cb4223a98e6b668dca330e925600f2699879d21b
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-01-26 19:46:27 -0300

    uupdated beneficiairo methods

--- app/controllers/beneficiarios_controller.rb
+++ app/controllers/beneficiarios_controller.rb
@@ -13,15 +13,40 @@ class BeneficiariosController < ApplicationController
   end
 
   def new
+    @beneficiario = Beneficiario.new
+    authorize @beneficiario
   end
 
   def edit
+    authorize @beneficiario
   end
 
   def create
+    @beneficiario = Beneficiario.new(beneficiario_params)
+    authorize @beneficiario
+
+    respond_to do |format|
+      if @beneficiario.save
+        format.html { redirect_to @beneficiario, notice: 'beneficiario was successfully created.' }
+        format.json { render :show, status: :created, location: @beneficiario }
+      else
+        format.html { render :new }
+        format.json { render json: @beneficiario.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      authorize @beneficiario
+      if @beneficiario.update(beneficiario_params)
+        format.html { redirect_to @beneficiario, notice: 'Beneficiario was successfully updated.' }
+        format.json { render :show, status: :ok, location: @beneficiario }
+      else
+        format.html { render :edit }
+        format.json { render json: @beneficiario.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private  
@@ -29,5 +54,37 @@ class BeneficiariosController < ApplicationController
   def set_beneficiario
     @beneficiario = Beneficiario.find(params[:id])
   end
+
+  def beneificario_params
+    attributes = [:id,
+    :email,
+    :encrypted_password,
+    :reset_password_token,
+    :reset_password_sent_at,
+    :remember_created_at,
+    :sign_in_count,
+    :current_sign_in_at,
+    :last_sign_in_at,
+    :current_sign_in_ip,
+    :last_sign_in_ip,
+    :nome,
+    :nacionalidade,
+    :cpf,
+    :sexo,
+    :data_nascimento,
+    :estado_civil,
+    :nome_da_mae,
+    :telefone,
+    :cargo,
+    :admissao,
+    :salario,
+    :papel,
+    :organizacao_id,
+    :created_at,
+    :updated_at,
+    :titular_id]
+    params.require(:beneficiario).permit(*attributes)
+  end
+
 
 end
