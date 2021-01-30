ef680a2f0ec788e82bb0d8ed3397d221f3c03411
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-01-30 19:45:14 -0300

    updated seed

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.safe_email}
+    first_name {Faker::Name.first_name }
+    last_name{ Faker::Name.last_name  }
+    email { Faker::Internet.safe_email(name: self.first_name)}
     password {"123123"}
+    phone {Faker::Number.number(digits: 11)}
     organizacao { nil }
   end
 end
