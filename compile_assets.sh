2195784739bed6221ad5d04665b548ff671e8d93
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-02-18 23:06:18 -0300

    production compiled assets for heroku assets pipeline

--- compile_assets.sh
+++ compile_assets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production bundle exec rake assets:precompile --trace
 git add . 
 git commit -m 'production compiled assets for heroku assets pipeline'
