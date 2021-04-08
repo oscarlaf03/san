d089d3a072de496b81b2f33e6baca9497bc9c1bc
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-04-07 22:00:41 -0300

    added Ticket org associations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,6 @@
 class Ticket < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   belongs_to :requestor, class_name: 'User', foreign_key: 'requestor_id'
+  delegate :organizacao, to: :requestor, allow_nil: true
+
 end
