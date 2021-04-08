3d323d75702e7b44a3432d0fece64db653ea9a82
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-04-08 10:04:37 -0300

    added validators to ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,25 @@
 class Ticket < ApplicationRecord
-  belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
+  belongs_to :owner, class_name: 'User', foreign_key: 'owner_id', optional: true
   belongs_to :requestor, class_name: 'User', foreign_key: 'requestor_id'
   delegate :organizacao, to: :requestor, allow_nil: true
+  validates :name_model, presence: true, constant: true
+  validates :action, presence: true, inclusion: { in: %w( create update delete),
+  message: "%{value} não é uma action valida"}
+  validates :id_model, presence: true, numericality: { only_integer: true }
+  validate :owner_is_internal_user, :requestor_is_organizacao_user
+
+  private
+
+  def owner_is_internal_user
+    if owner.present? && !owner.internal?
+      errors.add(:owner, "Só pode ser um usuário interno")
+    end
+  end
+
+  def requestor_is_organizacao_user
+    if requestor.present? && !requestor.organizacao?
+      errors.add(:requestor, "Só pode ser um usuário organizacao")
+    end
+  end
 
 end
